6928e09621b468e83fcd6412ba980163bf3c0eb1
Becky Zorn <bczorn@gmail.com>
2019-08-21 17:35:28 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   # your code here
-    array.each do |x|
-      square = Array.new {|x|x*x}
-      
+  square = Array.new {|i|i*i}  
+  array.each do |x|
+    
     end
 end
